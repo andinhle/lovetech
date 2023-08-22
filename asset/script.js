f38079c3548274f45c6a5edbc5b39f9defd9db6c
andinhle <81063603+andinhle@users.noreply.github.com>
2023-08-22 15:48:57 +0700

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -7,12 +7,3 @@ btnBars.addEventListener('click',function () {
 btnOut.addEventListener('click',function (){
     showMenu.classList.remove('show-menu-list');
 });
-var btnBars = document.querySelector('.bars');
-var showMenu = document.querySelector('.menu-list');
-var btnOut = document.querySelector('.menu-list li:last-child');
-btnBars.addEventListener('click',function () {
-    showMenu.classList.toggle('show-menu-list');
-});
-btnOut.addEventListener('click',function (){
-    showMenu.classList.remove('show-menu-list');
-});
