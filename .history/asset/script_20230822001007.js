04e1632eb253d57f89f4ec39debc609d30c18e54
Lê Đình An <ledinhan@MacBook-Air-cua-Le-4.local>
2023-08-22 15:00:30 +0700

    first commit

--- .history/asset/script_20230822001007.js
+++
@@ -1,9 +0,0 @@
-var btnBars = document.querySelector('.bars');
-var showMenu = document.querySelector('.menu-list');
-
-btnBars.addEventListener('click',function () {
-    showMenu.classList.toggle('show-menu-list');
-});
-btnOut.addEventListener('click',function (){
-    showMenu.classList.remove('show-menu-list');
-});
